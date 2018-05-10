dbb3d45e1a35e14dcebb7f38f6f42982a2f45b8b
masanori-10 <s.masanori.vx@gmail.com>
2018-05-10 16:34:01 +0900

    changed sample image (drag&drop -> button)

--- src/app.js
+++ src/app.js
@@ -180,6 +180,8 @@ function init() {
 }
 
 $(function () {
+    $('#source-text').val("X = <rocket>;\nfor(x of MObjects){\n    if(x.type == motype.IMAGE){\n        $x.y = x.y - 10;\n    }\n}");
+    var jsEditor = makeEditor();
     cvsw = $('#mapping-area').width();
     cvsh = $('#mapping-area').height();
     $('#cvs').attr('width', cvsw);
@@ -198,11 +200,12 @@ $(function () {
         }, 200);
     });
     $('#apply-source').click(function() {
-        jsEditor.save();
+        jsEditor.toTextArea();
         objPos = [0,0];
         var input = $('#source-text').val().toString();
         console.log(input);
         exec(input);
+        jsEditor = makeEditor();
     });
     $('#start-plot').click(function () {
         console.log("start");
@@ -224,94 +227,60 @@ $(function () {
         console.log("reset");
         init();
     });
+    $('#sakura').click(function (){
+        console.log("sakura");
+        jsEditor.toTextArea();
+        var input = $('#source-text').val().toString();
+        input = "_" + svariableCount + " = <sakura>;\n" + input;
+        svariableCount++;
+        $('#source-text').val(input);
+        jsEditor = makeEditor();
+    });
+    $('#fish').click(function (){
+        console.log("fish");
+        jsEditor.toTextArea();
+        var input = $('#source-text').val().toString();
+        input = "_" + svariableCount + " = <fish>;\n" + input;
+        svariableCount++;
+        $('#source-text').val(input);
+        jsEditor = makeEditor();
+    });
+    $('#star').click(function (){
+        console.log("star");
+        jsEditor.toTextArea();
+        var input = $('#source-text').val().toString();
+        input = "_" + svariableCount + " = <star>;\n" + input;
+        svariableCount++;
+        $('#source-text').val(input);
+        jsEditor = makeEditor();
+    });
+    $('#arrow').click(function (){
+        console.log("arrow");
+        jsEditor.toTextArea();
+        var input = $('#source-text').val().toString();
+        input = "_" + svariableCount + " = <arrow>;\n" + input;
+        svariableCount++;
+        $('#source-text').val(input);
+        jsEditor = makeEditor();
+    });
+    $('#rocket').click(function (){
+        console.log("rocket");
+        jsEditor.toTextArea();
+        var input = $('#source-text').val().toString();
+        input = "_" + svariableCount + " = <rocket>;\n" + input;
+        svariableCount++;
+        $('#source-text').val(input);
+        jsEditor = makeEditor();
+    });
 });
 
-(function(){
-
-    //要素の取得
-    var elements = document.getElementsByClassName("drag-and-drop");
-
-    //要素内のクリックされた位置を取得するグローバル（のような）変数
-    var x;
-    var y;
-
-    //マウスが要素内で押されたとき、又はタッチされたとき発火
-    for(var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mousedown", mdown, false);
-        elements[i].addEventListener("touchstart", mdown, false);
-    }
-
-    //マウスが押された際の関数
-    function mdown(e) {
-
-        //クラス名に .drag を追加
-        this.classList.add("drag");
-
-        //タッチデイベントとマウスのイベントの差異を吸収
-        if(e.type === "mousedown") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //要素内の相対座標を取得
-        x = event.pageX - this.offsetLeft;
-        y = event.pageY - this.offsetTop;
-
-        //ムーブイベントにコールバック
-        document.body.addEventListener("mousemove", mmove, false);
-        document.body.addEventListener("touchmove", mmove, false);
-    }
-
-    //マウスカーソルが動いたときに発火
-    function mmove(e) {
-
-        //ドラッグしている要素を取得
-        var drag = document.getElementsByClassName("drag")[0];
-
-        //同様にマウスとタッチの差異を吸収
-        if(e.type === "mousemove") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        }
-
-        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
-        e.preventDefault();
-
-        //マウスが動いた場所に要素を動かす
-        drag.style.top = event.pageY - y + "px";
-        drag.style.left = event.pageX - x + "px";
-
-        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
-        drag.addEventListener("mouseup", mup, false);
-        document.body.addEventListener("mouseleave", mup, false);
-        drag.addEventListener("touchend", mup, false);
-        document.body.addEventListener("touchleave", mup, false);
-
-    }
-
-    //マウスボタンが上がったら発火
-    function mup(e) {
-        var drag = document.getElementsByClassName("drag")[0];
-
-        //ムーブベントハンドラの消去
-        document.body.removeEventListener("mousemove", mmove, false);
-        drag.removeEventListener("mouseup", mup, false);
-        document.body.removeEventListener("touchmove", mmove, false);
-        drag.removeEventListener("touchend", mup, false);
-
-        //クラス名 .drag も消す
-        drag.classList.remove("drag");
-    }
-
-})()
-
-var jsEditor = CodeMirror.fromTextArea(document.getElementById("source-text"), {
-    mode: "javascript",
-    lineNumbers: false,
-    indentUnit: 4
-});
+function makeEditor(){
+    return CodeMirror.fromTextArea(document.getElementById("source-text"), {
+        mode: "javascript",
+        lineNumbers: false,
+        indentUnit: 4
+    });
+}
 
 function POS(posx,posy,obj){
     obj.x = posx;
