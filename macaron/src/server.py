27abb8f8b49d81a45dd842213fcbbd91419c7107
KASE <kaseyutaka.suffix@gmail.com>
2018-11-26 20:21:41 +0900

    modified server.py

--- macaron/src/server.py
+++ macaron/src/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, url_for, Response, send_file, render_template
-import json
-import re
-import os
+import json, re, os, platform, subprocess, sys
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -13,7 +12,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return send_file('templates/index.html')
+    return send_file(str(file_search('index.html', 'src/templates')))
+
+
 
 @app.route('/stylesheet', methods=['POST'])
 def transformStylesheet():
@@ -23,45 +24,62 @@ def transformStylesheet():
     # TODO トランスパイル
 
     if len(splitText) == 2:
-        with open('static/js/rule.js', mode='w') as f:
+        with file_search('rule.js', 'src/static/js/').open(mode='w') as f:
             f.write('var stylesheet = `' + splitText[0] + '`\n')
             f.write('function myRule(){' + splitText[1] + '}')
     else:
-        with open('static/js/rule.js', mode='w') as f:
+        with file_search('rule.js', 'src/static/js').open(mode='w') as f:
             f.write('var stylesheet = "";\nfunction myRule(){alert(\'syntax error\');}')
-        
-    return send_file('static/js/rule.js')
+
+    return send_file(str(file_search('rule.js', 'src/static/js')))
 
 
 @app.route('/sample/slingshot', methods=['POST'])
 def getSlingShotSample():
-    with open('../examples/slingshot.macaron') as f:
+    with file_search('slingshot.macaron').open() as f:
         return f.read()
 
 @app.route('/sample/bridge', methods=['POST'])
 def getBridgeSample():
-    with open('../examples/bridge.macaron') as f:
+    with file_search('bridge.macaron').open() as f:
         return f.read()
 
 @app.route('/sample/car', methods=['POST'])
 def getCarSample():
-    with open('../examples/car.macaron') as f:
+    with file_search('car.macaron').open() as f:
         return f.read()
 
 @app.route('/sample/pendulum', methods=['POST'])
 def getPendulumSample():
-    with open('../examples/pendulum.macaron') as f:
+    with file_search('pendulum.macaron').open() as f:
         return f.read()
 
 @app.route('/sample/wreckingball', methods=['POST'])
 def getWreckingBallSample():
-    with open('../examples/wreckingball.macaron') as f:
+    with file_search('wreckingball.macaron').open() as f:
         return f.read()
 
+def file_search(file, subdir = 'examples'):
+    return Path(__file__).resolve().parent.parent / subdir / file
+
 def main():
-    app.run(debug=True)
-    # app.debug = True # デバッグモード有効化
-    # app.run(host='0.0.0.0') # どこからでもアクセス可能に
+    argv = sys.argv
+    if len(argv) == 2 and argv[1] == 'update':
+        try:
+            subprocess.check_call(['pip3', 'install', '-U', 'git+https://github.com/t-ryo/macaron.git@server'])
+        except:
+            pass
+    else:
+        app.run(debug=True)
+        # app.debug = True # デバッグモード有効化
+        # app.run(host='0.0.0.0') # どこからでもアクセス可能に
+
+        if platform.system() == 'Darwin':
+            try:
+                #subprocess.check_call(['open'])
+                pass
+            except:
+                pass
 
 if __name__ == '__main__':
     main()
